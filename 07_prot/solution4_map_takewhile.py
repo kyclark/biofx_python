820ecd77d5d3c81f48d943b77e592d4268659267
Ken Youens-Clark <kyclark@gmail.com>
2020-11-14 15:23:36 -0700

    changed Stop to *

--- 07_prot/solution4_map_takewhile.py
+++ 07_prot/solution4_map_takewhile.py
@@ -53,18 +53,18 @@ def translate(rna: str) -> str:
         'GUC': 'V', 'GUG': 'V', 'GUU': 'V', 'UAC': 'Y', 'UAU': 'Y',
         'UCA': 'S', 'UCC': 'S', 'UCG': 'S', 'UCU': 'S', 'UGC': 'C',
         'UGG': 'W', 'UGU': 'C', 'UUA': 'L', 'UUC': 'F', 'UUG': 'L',
-        'UUU': 'F', 'UAA': 'Stop', 'UAG': 'Stop', 'UGA': 'Stop',
+        'UUU': 'F', 'UAA': '*', 'UAG': '*', 'UGA': '*',
     }
 
     # Method 3: L.C. -> map(), slice -> takewhile
     # aa = [codon_to_aa.get(codon, '-') for codon in codons(rna, 3)]
     aa = map(lambda codon: codon_to_aa.get(codon, '-'), codons(rna, 3))
 
-    # print(''.join(takewhile(lambda c: c != 'Stop', aa)))
-    # not_stop = partial(operator.ne, 'Stop')
+    # print(''.join(takewhile(lambda c: c != '*', aa)))
+    # not_stop = partial(operator.ne, '*')
     # return ''.join(takewhile(not_stop, aa))
 
-    return ''.join(takewhile(partial(operator.ne, 'Stop'), aa))
+    return ''.join(takewhile(partial(operator.ne, '*'), aa))
 
 
 # --------------------------------------------------
