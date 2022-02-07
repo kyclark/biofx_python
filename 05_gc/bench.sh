56914c757f8ed043fb377d65dc781041ef522c3a
chriztopherton <christopherton1998@gmail.com>
2022-02-06 18:37:51 -0800

    rm dir, upload pyspark solnution

--- 05_gc/bench.sh
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/env bash
-
-# Benchmark all the solutions
-
-PRGS=$(find . -name solution\* | sort | xargs echo | sed "s/ /,/g")
-hyperfine -i --warmup 1 -L prg $PRGS '{prg} seqs.fa'
--- 09_grph/log.py
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env python3
-
-import sys
-
-print('This is STDOUT.')
-print('This is also STDOUT.', file=sys.stdout)
-print('This is STDERR.', file=sys.stderr)
